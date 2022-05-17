7c0ae27bb1338600a9dd7bafbfcdd87b6aae6b58
birosbeni <Beni>
2022-05-17 19:20:17 +0200

    hitelesítés
    Hitelesítés hozz adása a bejelentkezéshez

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { RootComponent } from './root/root.component';
@@ -18,6 +18,9 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RecipeService } from './recipes/recipe.service';
+import { AuthComponent } from './auth/auth.component';
+import { LoadingSpinnerComponent } from './shared/loadin-spinner/loadin-spinner.component';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -32,7 +35,9 @@ import { RecipeService } from './recipes/recipe.service';
     ShoppingEditComponent,
     DropdownDirective,
     RecipeStartComponent,
-    RecipeEditComponent
+    RecipeEditComponent,
+    AuthComponent,
+    LoadingSpinnerComponent
   ],
   imports: [
     BrowserModule,
@@ -41,7 +46,7 @@ import { RecipeService } from './recipes/recipe.service';
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [ShoppingListService, RecipeService],
+  providers: [ShoppingListService, RecipeService, { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map, tap } from "rxjs";
+import { exhaustMap, map, take, tap } from "rxjs";
+import { AuthService } from "../auth/auth.service";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
@@ -8,7 +9,7 @@ import { RecipeService } from "../recipes/recipe.service";
     providedIn: 'root'
 })
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipeService: RecipeService) {
+    constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {
 
     }
 
@@ -24,17 +25,18 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.http
-            .get<Recipe[]>('https://http-request-tutorial-3d4f6-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
-            .pipe(map(recipes => {
+        //take-ell nem kell unsubsribeolni
+
+        return this.http.get<Recipe[]>(
+            'https://http-request-tutorial-3d4f6-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
+        ).pipe(
+            map(recipes => {
                 return recipes.map(recipe => {
                     return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
                 });
             }),
-                tap(recipes => {
-                    this.recipeService.setRecipes(recipes);
-                })
-            )
-
+            tap(recipes => {
+                this.recipeService.setRecipes(recipes);
+            }));
     }
 }
